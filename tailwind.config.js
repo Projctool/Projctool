fa929b7c84eccfed817fa7f02db6cb57a02198fb
jackby03 <jackdelaguilavilla03@gmail.com>
2024-08-10 01:56:25 -0500

    fix configuration project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
       extend: {
         colors: {
           'primary': '#1B4E6E',
-          
           'whitegray': '#F5F7FA',
           'lightgray': '#E0E3E9',
           grayblue: '#8E9AAB',
